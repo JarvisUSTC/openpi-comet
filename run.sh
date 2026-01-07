c82a9d4965f88069b6c133d20e8d696559f0b4d9
JarvisUSTC <jarvisustc@gmail.com>
2026-01-07 11:54:43 +0800

    local changes 0107

---
+++ run.sh
@@ -0,0 +1,2 @@
+python scripts/serve_b1k.py --task_name=turning_on_radio --record --control_mode=receeding_horizon --dataset_root=/home/simpleai/ruike/2025-challenge-demos policy:checkpoint --policy.config=pi05_b1k-pt50_cs32_bs64_lr2.5e-5_step50k --policy.dir=/home/simpleai/ruike/task_checkpoint/openpi_comet/pi05-b1kpt50-cs32
+
